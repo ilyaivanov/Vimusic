3ffc1bf1a878d745c452d86a36f66069c71f61b9
Ilya <illiai@wix.com>
2019-03-01 13:27:44 +0200

    Extract generated code

---
+++ src/api/youtube.generated.ts
@@ -0,0 +1,58 @@
+//GENERATED CODE FROM SAMPLE RESPONSE
+export interface RootObject {
+  kind: string;
+  etag: string;
+  nextPageToken: string;
+  regionCode: string;
+  pageInfo: PageInfo;
+  items: ItemsItem[];
+}
+
+interface PageInfo {
+  totalResults: number;
+  resultsPerPage: number;
+}
+
+interface ItemsItem {
+  kind: string;
+  etag: string;
+  id: Id;
+  snippet: Snippet;
+}
+
+interface Id {
+  kind: string;
+  videoId: string;
+}
+
+interface Snippet {
+  publishedAt: string;
+  channelId: string;
+  title: string;
+  description: string;
+  thumbnails: Thumbnails;
+  channelTitle: string;
+  liveBroadcastContent: string;
+}
+
+interface Thumbnails {
+  default: {
+    url: string;
+    width: number;
+    height: number;
+  };
+  medium: Medium;
+  high: High;
+}
+
+interface Medium {
+  url: string;
+  width: number;
+  height: number;
+}
+
+interface High {
+  url: string;
+  width: number;
+  height: number;
+}
--- src/api/youtube.ts
+++ src/api/youtube.ts
@@ -1,74 +1,16 @@
-import { Video } from "./index";
+import {Video} from "./index";
+import {RootObject} from "./youtube.generated";
 
 export const searchVideos = (term: string): Promise<Video[]> =>
   fetch(
     "https://www.googleapis.com/youtube/v3/search?part=snippet&shart=mostPopular&maxResults=10&key=AIzaSyBsCL-zrXWd9S2FKRSDVfz7dOo783LQkLk&q=" +
-      term
+    term
   )
     .then(response => response.json())
     .then((data: RootObject) =>
       data.items
         .filter(v => v.id.videoId)
         .map(s => {
-          return { text: s.snippet.title, id: s.id.videoId };
+          return {text: s.snippet.title, id: s.id.videoId};
         })
     );
-
-//GENERATED CODE FROM SAMPLE RESPONSE
-interface RootObject {
-  kind: string;
-  etag: string;
-  nextPageToken: string;
-  regionCode: string;
-  pageInfo: PageInfo;
-  items: ItemsItem[];
-}
-
-interface PageInfo {
-  totalResults: number;
-  resultsPerPage: number;
-}
-
-interface ItemsItem {
-  kind: string;
-  etag: string;
-  id: Id;
-  snippet: Snippet;
-}
-
-interface Id {
-  kind: string;
-  videoId: string;
-}
-
-interface Snippet {
-  publishedAt: string;
-  channelId: string;
-  title: string;
-  description: string;
-  thumbnails: Thumbnails;
-  channelTitle: string;
-  liveBroadcastContent: string;
-}
-
-interface Thumbnails {
-  default: {
-    url: string;
-    width: number;
-    height: number;
-  };
-  medium: Medium;
-  high: High;
-}
-
-interface Medium {
-  url: string;
-  width: number;
-  height: number;
-}
-
-interface High {
-  url: string;
-  width: number;
-  height: number;
-}
