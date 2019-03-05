b50f6f71c56b1a9ee886a85d77c7825e4a1ac27f
Ilya <illiai@wix.com>
2019-03-05 13:24:25 +0200

    Updates

---
+++ precache-manifest.eb34f4a1cde9b06bc111dbcc91b0a852.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "954bb493f1604bbbd41f",
+    "url": "/Vimusic/static/js/runtime~main.954bb493.js"
+  },
+  {
+    "revision": "092cbdb3799c394382b7",
+    "url": "/Vimusic/static/js/main.092cbdb3.chunk.js"
+  },
+  {
+    "revision": "8d760a9e16148925f448",
+    "url": "/Vimusic/static/js/2.8d760a9e.chunk.js"
+  },
+  {
+    "revision": "092cbdb3799c394382b7",
+    "url": "/Vimusic/static/css/main.c1652f47.chunk.css"
+  },
+  {
+    "revision": "25000f87dd41c2e88c1e6bbc393494f1",
+    "url": "/Vimusic/index.html"
+  }
+];
